e3fba4f905ac97d6c298f03b450e78d15f2807ce
Moyasi Ginko <mrsiyam61@gmail.com>
2023-06-28 01:15:23 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// /* eslint-disable */
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import MyProfile from './components/myProfile';
+import About from './components/about';
+import NotFound from './components/notFound';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Home title="Welcome to React" />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/profile" element={<MyProfile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Home title="Welcome to React" />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
